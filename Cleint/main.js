960ecfad43c54f796c2d633424bec15b0be3c2c8
UP953058 <UP953068@myport.ac.uk>
2023-06-15 11:24:19 +0100

    Added clickable placeholders to work section

--- Cleint/main.js
+++ Cleint/main.js
@@ -21,8 +21,53 @@ window.onscroll = function () {
     prevScrollpos = currentScrollPos;
 }
 
+function workBoxClick() {
+
+    // get each individual box and add event listener
+    const box1 = document.querySelector(".workBox1");
+    const box1Content = document.querySelector(".hiddenContent");
+    box1.addEventListener("click", () => {
+        box1.classList.toggle("active");
+        box1Content.classList.toggle("hiddenContentDisplay");
+
+
+    });
+
+    const box2 = document.querySelector(".workBox2");
+    box2.addEventListener("click", () => {
+        box2.classList.toggle("active");
+
+    });
+
+    const box3 = document.querySelector(".workBox3");
+    box3.addEventListener("click", () => {
+        box3.classList.toggle("active");
+
+    });
+
+    const box4 = document.querySelector(".workBox4");
+    box4.addEventListener("click", () => {
+        box4.classList.toggle("active");
+
+    });
+
+    const box5 = document.querySelector(".workBox5");
+    box5.addEventListener("click", () => {
+        box5.classList.toggle("active");
+
+    });
+
+    const box6 = document.querySelector(".workBox6");
+    box6.addEventListener("click", () => {
+        box6.classList.toggle("active");
+
+    });
+}
+
+
 
 function init() {
+    workBoxClick();
     section1();
 }
 
