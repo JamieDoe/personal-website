0250ab276e70c09eddd072517fc9a56abe1b0285
UP953058 <UP953068@myport.ac.uk>
2023-07-05 16:46:43 +0100

    added carousel for work section and re designed contact form

--- Cleint/main.js
+++ Cleint/main.js
@@ -21,31 +21,70 @@ window.onscroll = function () {
     prevScrollpos = currentScrollPos;
 }
 
-function onHover() {
-    // on hover of work boxes, display their unique hiddenContent
-    let workBoxes = document.querySelectorAll("#workBoxes");
-    let hiddenContent = document.querySelectorAll(".hiddenContent");
+function formEmail() {
 
-    for (let i = 0; i < workBoxes.length; i++) {
-        workBoxes[i].addEventListener("mouseover", function () {
-            hiddenContent[i].style.display = "block";
-            // also give hiddenContent a display of grid
-            hiddenContent[i].style.display = "grid";
-            hiddenContent[i].style.gridTemplateColumns = "1fr";
-            hiddenContent[i].style.gridTemplateRows = "1fr 3fr 1fr";
-        });
-        workBoxes[i].addEventListener("mouseout", function () {
-            hiddenContent[i].style.display = "none";
-        });
-    }
+    const submitBtn = document.querySelector("#submit");
+    submitBtn.addEventListener("click", function(e) {
+        e.preventDefault();
+        let selector = document.querySelector("#selector").value;
+        let name = document.querySelector("#name").value;
+        let email = document.querySelector("#email").value;
+        let message = document.querySelector("#message").value;
+        let body = 'Service: ' + selector + '<br>' + 'name: ' + name + '<br>' + 'email: ' + email + '<br>' + 'message: ' + message;
+        
+        Email.send({
+            SecureToken : "285231fa-b690-4f18-a029-96f090cf63f0",
+            To : 'jamieoliverdoe@gmail.com',
+            From : 'jamieoliverdoe@gmail.com',
+            Subject : "This is the subject",
+            Body : body,
+        }).then(
+          alert("Thank you for contacting me! I will get back to you as soon as possible."),
+        );
+    });
 }
 
+function carousel() {
+    const carousel = document.querySelector(".wrapper");
+    firstImg = carousel.querySelectorAll("img")[0];
+    arrowIcons = document.querySelectorAll("#work i");
 
+    let isDragStart = false, prevPageX, prevScrollLeft;
+    let firstImgWidth = firstImg.clientWidth + 14;
+
+    arrowIcons.forEach(icon => {
+        icon.addEventListener("click", () => {
+            carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        });
+    });
+
+    const dragStart = (e) => {
+        isDragStart = true;
+        prevPageX = e.pageX;
+        prevScrollLeft = carousel.scrollLeft;
+    }
+
+    const dragging = (e) => {
+        if(!isDragStart) return;
+        e.preventDefault();
+        let positionDiff = e.pageX - prevPageX;
+        carousel.scrollLeft = prevScrollLeft - positionDiff;
+    }
+
+    const dragStop = () => {
+        isDragStart = false;
+    }
+    
+    carousel.addEventListener("mousemove", dragging);
+    carousel.addEventListener("mousedown", dragStart);
+    carousel.addEventListener("mouseup", dragStop);
+}
 
 function init() {
     //alert("This website is still under construction. It is only responsive for screens 1080p and above at the moment... Excuse the mess!");
     section1();
-    onHover();
+    carousel();
+    formEmail();
 }
 
 window.onload = init();
